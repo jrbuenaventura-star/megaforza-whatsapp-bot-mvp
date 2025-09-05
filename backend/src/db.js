b62dc3a4581d0c936c8fa2ea48a41835ce7d081e
jrbuenaventura-star <jrbuenaventura@gmail.com>
2025-09-05 13:12:36 -0500

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,2 +1,9 @@
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+// backend/src/db.js
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis;
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}

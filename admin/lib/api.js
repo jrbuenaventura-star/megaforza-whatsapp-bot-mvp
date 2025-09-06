5eaddd4e4f1df2502ec581abda9092d4ae218675
jrbuenaventura-star <jrbuenaventura@gmail.com>
2025-09-05 21:53:29 -0500

    Update api.js

--- admin/lib/api.js
+++ admin/lib/api.js
@@ -1,1 +1,21 @@
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 
+if (!API_BASE) {
+  console.warn("NEXT_PUBLIC_API_BASE no está definido");
+}
+
+export async function api(path, init) {
+  const url = `${API_BASE}${path}`;
+  const res = await fetch(url, {
+    ...init,
+    headers: {
+      "Content-Type": "application/json",
+      ...(init && init.headers ? init.headers : {}),
+    },
+  });
+  if (!res.ok) {
+    const text = await res.text().catch(() => "");
+    throw new Error(`API ${res.status} ${res.statusText}: ${text}`);
+  }
+  return res.json();
+}
